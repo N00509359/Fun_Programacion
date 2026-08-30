b7f7a0e1f3eb8e4277c2b0d1b3e6692deacc5267
Raquel V. <n00509359@upn.pe>
2026-08-29 20:02:46 -0500

    Sintaxis entrada, salida, comentarios en Python

--- Python/0.fundamentos.py
+++ Python/0.fundamentos.py
@@ -1,1 +1,30 @@
 # Fundamentos de lenguaje Python
+
+# Numero Entero
+edad = 27
+
+# Cadena de texto
+nombre = "Raquel"
+
+# Numero Decimal
+precio = 19.99
+
+# Valor Lógico
+estado = False
+
+# Valor caracter
+genero = 'M'
+
+# Entrada de Datos
+nombre = imput("Ingrese su nombre")
+
+# Salida de Datos
+print (f'Hola, {nombre}')
+
+# Comentario en bloque
+'''
+Comentario en Bloque
+'''
+
+# Ejecutar programa Python
+python fundamentos.py
