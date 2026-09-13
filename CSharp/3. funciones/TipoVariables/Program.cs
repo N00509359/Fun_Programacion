4ae3f34cd9de4264dd7c20fc6f8986c3644fecd7
Raquel Valdez <N00509359@upn.pe>
2026-09-13 09:56:38 -0500

    Variable global-compartida, no local en C#

---
+++ CSharp/3. funciones/TipoVariables/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("--tipos de variables --");
+static void saludar()
+{
+    string nombre ="Raquel";
+    Console.WriteLine($"hola {nombre}, bienvenido");
+}
+saludar();
+//Console.WriteLine(nombre);
+
+Console.WriteLine("variable global - compartida");
+int incrementar=0;
+static void incrementa(int a)
+{
+    a++;
+    Console.WriteLine($"el valor de incrementar es: {a}");
+}
+incrementa(incrementar);
+incrementa(incrementar);
+
+
+Console.WriteLine("variable no local");
+static void calcularMonto()
+{
+    double subtotal = 100;
+    double calcular_igv(double a)
+    {
+        return a*0.18;
+    }
+    double resultado = calcular_igv(subtotal);
+    Console.WriteLine($"el resultado subtotal es: {resultado + subtotal}");
+}
+calcularMonto();
